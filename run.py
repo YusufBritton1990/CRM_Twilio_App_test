de8dcce63ae55e5c48d5990381faae171a4f51db
Yusuf Britton <yusufbritton@yahoo.com>
2019-09-03 07:56:11 -0400

    Test SMS reply complete

--- run.py
+++ run.py
@@ -1,9 +1,20 @@
-from flask import Flask
+from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingResponse
+
 app = Flask(__name__)
 
-@app.route("/sms")
-def hello():
-    return "Hello World!"
+# https://www.twilio.com/docs/sms/quickstart/python#install-python-and-the-twilio-helper-library
+
+@app.route("/sms", methods=['GET', 'POST'])
+def sms_ahoy_reply():
+    """Respond to incoming messages with a friendly SMS."""
+    # Start our response
+    resp = MessagingResponse()
+
+    # Add a message
+    resp.message("Ahoy! Thanks so much for your message.")
+
+    return str(resp)
 
 if __name__ == "__main__":
     app.run(debug=True)
